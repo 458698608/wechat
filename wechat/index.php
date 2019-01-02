b95eebc8b596775a7462340a1b692c2bdaf5e1e7
458698608 <458698608@qq.com>
2019-01-02 16:15:55 +0800

    first commit

---
+++ wechat/index.php
@@ -0,0 +1,31 @@
+<?php
+ 2 define("weixin", "此处填写你设置的token值");
+ 3
+ 4 $wechatObj = new CallbackAPI;
+ 5 $wechatObj->valid();
+ 6
+ 7 class CallbackAPI {
+ 8
+ 9     /**
+10      * 签名验证
+11      * @return [type] [description]
+12      */
+13      public function valid() {
+14         $echoStr = $_GET["echostr"];
+15         $signature = $_GET["signature"];
+16         $timestamp = $_GET["timestamp"];
+17         $nonce = $_GET["nonce"];
+18         $token = TOKEN;
+19         //将token、timestamp、nonce按字典序排序
+20         $tmpArr = array($token, $timestamp, $nonce);
+21         sort($tmpArr);
+22         $tmpStr = implode($tmpArr);
+23         //对tmpStr进行sha1加密
+24         $tmpStr = sha1($tmpStr);
+25         if($tmpStr == $signature){
+26             header('content-type:text');
+27             echo $echoStr;
+28             exit;
+29         }
+30     }
+31 }
